cb836d9aa78a19a38a0ba00e1adac2b7795788a0
eduardo.hernandez <eduardoha70@gmail.com>
2018-06-20 11:58:56 -0500

    (feat) Instalacion e implementacion de auditoria para modelos

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -7,17 +7,42 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Webpatser\Uuid\Uuid;
 
-class Producto extends Ardent
-{
-    use SoftDeletes;
+use OwenIt\Auditing\Auditable;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 
+class Producto extends Ardent implements AuditableContract
+{
+    use Auditable, SoftDeletes;
+
+    /**
+     * Atributo de tabla del modelo
+     *
+     * @var array
+     */
     protected $table = "producto";
 
+    /**
+     * Atributo de conexion a base de datos
+     *
+     * @var array
+     */
+    protected $connection = 'data';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'descripcion',
         'precio',
     ];
 
+    /**
+     * Reglas para los campos del modelo
+     *
+     * @var array
+     */
     public static $rules = [
         'descripcion' => 'required|between:3,80',
         'precio'      => 'required|integer',
