5961b1d527f809b37c82a1453eafd1bc17d95544
eduardo.hernandez <eduardoha70@gmail.com>
2018-09-04 13:15:47 -0500

    correcciones

--- start.sql
+++ start.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 DROP DATABASE proyecto_laravel;
 CREATE DATABASE proyecto_laravel;
 
@@ -8,8 +8,4 @@ CREATE DATABASE audits;
 DROP DATABASE public;
 CREATE DATABASE public;
 
-=======
-CREATE DATABASE proyecto_laravel;
-
->>>>>>> c6689236a5d9044bd60278bbbd8130ef04139b5c
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Hern@ndez1';
